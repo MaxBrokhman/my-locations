d778117cd3f88b68d056be1fe26c931838ff88f4
MaxBrohman <maksimbrochman@gmail.com>
2020-07-02 19:10:57 +0300

    removed dash in url

--- src/components/App/config.js
+++ src/components/App/config.js
@@ -5,5 +5,5 @@ const URL_PREFIX = window.location.pathname.match(urlCheck)
   : ''
 
 export const MAIN_PATHNAME = `${URL_PREFIX}`
-export const CATEGORY_DETAILS_PATHNAME = `${URL_PREFIX}/active-category`
-export const NEW_CATEGORY_PATHNAME = `${URL_PREFIX}/new-category`
+export const CATEGORY_DETAILS_PATHNAME = `${URL_PREFIX}active-category`
+export const NEW_CATEGORY_PATHNAME = `${URL_PREFIX}new-category`
