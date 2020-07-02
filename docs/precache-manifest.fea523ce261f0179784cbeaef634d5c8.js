6c6afe2e49a6fbbae5462ab022106ed944a1f835
MaxBrohman <maksimbrochman@gmail.com>
2020-07-02 09:50:51 +0300

    docs folder added

---
+++ docs/precache-manifest.fea523ce261f0179784cbeaef634d5c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f65cf48d4de6a7bc2f71b559064d86",
+    "url": "/my-locations/index.html"
+  },
+  {
+    "revision": "9858687f5e3fc737b5d9",
+    "url": "/my-locations/static/css/main.d01eefd1.chunk.css"
+  },
+  {
+    "revision": "340c29d866d434631e9f",
+    "url": "/my-locations/static/js/2.a8c877b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-locations/static/js/2.a8c877b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9858687f5e3fc737b5d9",
+    "url": "/my-locations/static/js/main.3455d4ab.chunk.js"
+  },
+  {
+    "revision": "1c3c54859202669d1ff0",
+    "url": "/my-locations/static/js/runtime-main.6aeb2177.js"
+  },
+  {
+    "revision": "925864839f5cd68d0e8cdd443d32b355",
+    "url": "/my-locations/static/media/add_circle-black-18dp.92586483.svg"
+  },
+  {
+    "revision": "56397e007532c05f39537f2a5b538cf1",
+    "url": "/my-locations/static/media/delete-black-18dp.56397e00.svg"
+  },
+  {
+    "revision": "2de08358659a7982fe1b2049fd519c45",
+    "url": "/my-locations/static/media/edit-black-18dp.2de08358.svg"
+  },
+  {
+    "revision": "0559cea496bc8c2e71ca745cce282ef0",
+    "url": "/my-locations/static/media/preview-black-18dp.0559cea4.svg"
+  }
+]);
